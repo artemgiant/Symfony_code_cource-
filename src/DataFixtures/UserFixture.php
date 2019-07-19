6c3c10207fef7dcd3a1bc1706d84894a75703b19
Artem <artemgiant@gmail.com>
2019-07-19 12:12:16 +0300

    Password добавления проверка

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,9 +5,19 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     protected function loadData(ObjectManager $manager)
     {
         // $product = new Product();
@@ -17,7 +27,7 @@ class UserFixture extends BaseFixture
             $user = new User();
             $user->setEmail(sprintf('user%d@test.com',$i));
             $user->setFirstName($this->faker->firstName);
-
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'test'));
             return $user;
 
         });
